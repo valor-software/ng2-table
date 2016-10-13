a63395335a7d2a011c4f6781f4336a9d81f2e2ff
Daniel Cooke <cooked@AMT.internal>
2016-10-13 16:32:40 +0100

    fuhwe

--- gulp-tasks/lint.js
+++ gulp-tasks/lint.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const tslint = require('gulp-tslint');
 const gitignore = require('gitignore-to-glob')();
 
-gitignore.push('**/*.ts');
+//gitignore.push('**/*.ts');
 
 gulp.task('tslint', () =>
   gulp
