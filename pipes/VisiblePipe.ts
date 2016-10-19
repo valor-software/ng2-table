d492c2f2e1d251c2037a3612929986b40df7d3f1
Matthew Vaughan <m0v0nage@gmail.com>
2016-10-19 09:52:48 -0600

    Visible pipe

---
+++ pipes/VisiblePipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform, Injectable } from '@angular/core';
+
+@Pipe({
+  name: 'visible',
+  pure: false
+})
+@Injectable()
+export class VisiblePipe implements PipeTransform {
+  public transform(value: Array<any>):Array<any> {
+    return value.filter((c:any) => c.visible);
+  }
+}
