1ce28a2a4ad5cc582e94c5e0866ec5d788f8a3e9
wdunn001 <wdunn001@gmail.com>
2017-04-12 10:27:04 -0400

    add a double click event option to table
    a double click event option added to table so people can highlight text and double click

--- components/table/ng-table.component.spec.ts
+++ components/table/ng-table.component.spec.ts
@@ -1,38 +1,127 @@
-import { Component } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { Ng2TableModule } from '../ng-table-module';
-
-const html = ``;
-
-describe('Component: ng2-table', () => {
-  let fixture:ComponentFixture<any>;
-  let context:TestTableComponent;
-  let element:any;
-  let clean:any;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TestTableComponent],
-      imports: [Ng2TableModule]
-    });
-    TestBed.overrideComponent(TestTableComponent, {set: {template: html}});
-    fixture = TestBed.createComponent(TestTableComponent);
-    context = fixture.componentInstance;
-    element = fixture.nativeElement.querySelector('#c1');
-    clean = fixture.nativeElement.querySelector('#c2');
-    fixture.detectChanges();
-  });
-
-  it('should be true', () => {
-    expect(true).toBe(true);
-  });
-});
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
-  selector: 'table-test',
-  template: ''
+    selector: 'ng-table',
+    template: `
+    <table class="table dataTable" ngClass="{{config.className || ''}}"
+           role="grid" style="width: 100%;">
+      <thead>
+        <tr role="row">
+          <th *ngFor="let column of columns" [ngTableSorting]="config" [column]="column" 
+              (sortChanged)="onChangeTable($event)" ngClass="{{column.className || ''}}">
+            {{column.title}}
+            <i *ngIf="config && column.sort" class="pull-right fa"
+              [ngClass]="{'fa-chevron-down': column.sort === 'desc', 'fa-chevron-up': column.sort === 'asc'}"></i>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr *ngIf="showFilterRow">
+        <td *ngFor="let column of columns">
+          <input *ngIf="column.filtering" placeholder="{{column.filtering.placeholder}}"
+                 [ngTableFiltering]="column.filtering"
+                 class="form-control"
+                 style="width: auto;"
+                 (tableChanged)="onChangeTable(config)"/>
+        </td>
+      </tr>
+        <tr *ngFor="let row of rows">
+          <td (click)="cellClick(row, column.name)" (dblclick)="cellDoubleClick(row, column.name)" *ngFor="let column of columns" [innerHtml]="sanitize(getData(row, column.name))"></td>
+        </tr>
+      </tbody>
+    </table>
+  `
 })
+export class NgTableComponent {
+    // Table values
+    @Input() public rows: Array<any> = [];
 
-class TestTableComponent {
+    @Input()
+    public set config(conf: any) {
+        if (!conf.className) {
+            conf.className = 'table-striped table-bordered';
+        }
+        if (conf.className instanceof Array) {
+            conf.className = conf.className.join(' ');
+        }
+        this._config = conf;
+    }
+
+    // Outputs (Events)
+    @Output() public tableChanged: EventEmitter<any> = new EventEmitter();
+    @Output() public cellClicked: EventEmitter<any> = new EventEmitter();
+    @Output() public cellDoubleClicked: EventEmitter<any> = new EventEmitter();
+
+    public showFilterRow: Boolean = false;
+
+    @Input()
+    public set columns(values: Array<any>) {
+        values.forEach((value: any) => {
+            if (value.filtering) {
+                this.showFilterRow = true;
+            }
+            if (value.className && value.className instanceof Array) {
+                value.className = value.className.join(' ');
+            }
+            let column = this._columns.find((col: any) => col.name === value.name);
+            if (column) {
+                Object.assign(column, value);
+            }
+            if (!column) {
+                this._columns.push(value);
+            }
+        });
+    }
+
+    private _columns: Array<any> = [];
+    private _config: any = {};
+
+    public constructor(private sanitizer: DomSanitizer) {
+    }
+
+    public sanitize(html: string): SafeHtml {
+        return this.sanitizer.bypassSecurityTrustHtml(html);
+    }
+
+    public get columns(): Array<any> {
+        return this._columns;
+    }
+
+    public get config(): any {
+        return this._config;
+    }
+
+    public get configColumns(): any {
+        let sortColumns: Array<any> = [];
+
+        this.columns.forEach((column: any) => {
+            if (column.sort) {
+                sortColumns.push(column);
+            }
+        });
+
+        return { columns: sortColumns };
+    }
+
+    public onChangeTable(column: any): void {
+        this._columns.forEach((col: any) => {
+            if (col.name !== column.name && col.sort !== false) {
+                col.sort = '';
+            }
+        });
+        this.tableChanged.emit({ sorting: this.configColumns });
+    }
+
+    public getData(row: any, propertyName: string): string {
+        return propertyName.split('.').reduce((prev: any, curr: string) => prev[curr], row);
+    }
+
+    public cellClick(row: any, column: any): void {
+        this.cellClicked.emit({ row, column });
+    }
+
+    public cellDoubleClick(row: any, column: any): void {
+        this.cellDoubleClicked.emit({ row, column });
+    }
 }
